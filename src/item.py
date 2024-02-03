284bb3acb0dedf183d86d8193a8e5f553cb68315
kasyan1337 <tristokorun@gmail.com>
2024-02-04 00:41:25 +0800

    homework 2 - Done

--- src/item.py
+++ src/item.py
@@ -1,10 +1,13 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -17,13 +20,23 @@ class Item:
         assert price >= 0, f"Price {price} should be more than 0"
         assert quantity >= 0, f"Quantity {quantity} should be more than 0"
 
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         # Добавляем созданный экземпляр в список всех товаров
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) < 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -32,9 +45,23 @@ class Item:
         """
         return self.price * self.quantity
 
-
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path=None):
+        cls.all = []
+        file_path = '../src/items.csv'
+        with open(file_path, mode='r', encoding='windows-1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                cls(name=row['name'], price=float(row['price']), quantity=int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(number_str):
+        number = int(float(number_str))
+        return number
+
